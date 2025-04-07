d2f0f6b84f9c6273adcfd2f0a5b10eb571b2464d
n4nm4r <nancy.martin.nb@gmail.com>
2025-04-07 10:55:10 -0300

    Add project files.

---
+++ assigment_4_IMDB/Models/Generated/Episode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assigment_4_IMDB.Models;
+
+public partial class Episode
+{
+    public string TitleId { get; set; } = null!;
+
+    public string? ParentTitleId { get; set; }
+
+    public int? SeasonNumber { get; set; }
+
+    public int? EpisodeNumber { get; set; }
+
+    public virtual Title? ParentTitle { get; set; }
+
+    public virtual Title Title { get; set; } = null!;
+}
---
+++ assigment_4_IMDB/Models/Generated/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assigment_4_IMDB.Models;
+
+public partial class Rating
+{
+    public string TitleId { get; set; } = null!;
+
+    public decimal? AverageRating { get; set; }
+
+    public int? NumVotes { get; set; }
+
+    public virtual Title Title { get; set; } = null!;
+}
