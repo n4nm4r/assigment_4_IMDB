39235a6f43511d4731a46f8a9b71221c103a00ad
n4nm4r <nancy.martin.nb@gmail.com>
2025-04-12 13:21:50 -0300

    trying to show episodes

--- assigment_4_IMDB/ViewModels/SeriesDetailsViewModel.cs
+++ assigment_4_IMDB/ViewModels/SeriesDetailsViewModel.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace assigment_4_IMDB.ViewModels
 {
@@ -11,29 +9,28 @@ namespace assigment_4_IMDB.ViewModels
     {
         public string TitleName { get; set; }
         public int Year { get; set; }
-        public List<string> ActorNames { get; set; }
-        public List<int> RatingStars { get; set; }  // Used for binding stars
+
+        // Stars to display (e.g., 7 means 7 filled stars)
+        public List<int> RatingStars { get; set; }
+
+        // List of episodes for this series
         public List<Episode> Episodes { get; set; }
 
         public SeriesDetailsViewModel(Title selectedTitle)
         {
+            // Title and year
             TitleName = selectedTitle.PrimaryTitle;
             Year = selectedTitle.StartYear ?? 0;
-            ActorNames = selectedTitle.Names.Select(n => n.PrimaryName).ToList();
 
+            // Rating stars
             var avgRating = selectedTitle.Rating?.AverageRating ?? 0;
             RatingStars = Enumerable.Range(1, (int)Math.Round(avgRating)).ToList();
 
+            // Episodes ordered by season and episode number
             Episodes = selectedTitle.EpisodeParentTitles
                 .OrderBy(e => e.SeasonNumber)
                 .ThenBy(e => e.EpisodeNumber)
                 .ToList();
         }
-
-
-
-
     }
-
-
 }
