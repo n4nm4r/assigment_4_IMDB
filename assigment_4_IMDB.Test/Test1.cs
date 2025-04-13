e9dbfdfc0808231631c160c1a051d307bfbd5310
n4nm4r <nancy.martin.nb@gmail.com>
2025-04-13 15:08:24 -0300

    changed properties of Unit test, added amin window initializer

--- assigment_4_IMDB.Test/Test1.cs
+++ assigment_4_IMDB.Test/Test1.cs
@@ -3,6 +3,15 @@
     [TestClass]
     public sealed class Test1
     {
+
+        private MainWindow _mainWindow;
+
+        [TestInitialize]
+        public void InitializeAllTests()
+        {
+            _mainWindow = new MainWindow();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
